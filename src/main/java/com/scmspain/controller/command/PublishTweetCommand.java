6ad91e0be6bcaf5a8898a24e157254867a9bbd5f
ivan-perez <ivan.perez@netcentric.biz>
2018-04-15 12:44:15 +0200

    Cleaning the publish tweet command.

--- src/main/java/com/scmspain/controller/command/PublishTweetCommand.java
+++ src/main/java/com/scmspain/controller/command/PublishTweetCommand.java
@@ -1,24 +1,29 @@
 package com.scmspain.controller.command;
 
+/**
+ * Command for publish a tweet.
+ */
 public class PublishTweetCommand {
 
     private String publisher;
     private String tweet;
 
+    /**
+     * Gets the publisher of the tweet to be published.
+     *
+     * @return Publisher.
+     */
     public String getPublisher() {
         return publisher;
     }
 
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
+    /**
+     * Gets the tweet text of the tweet to be published.
+     *
+     * @return Tweet text.
+     */
     public String getTweet() {
         return tweet;
-    }
-
-    public void setTweet(String tweet) {
-        this.tweet = tweet;
     }
 
 }
